fcc01e16f1c4e30fddf940ba41aa5898d32d85ac
Rios-Albert-Lee1905 <112599223+Rios-Albert-Lee1905@users.noreply.github.com>
2025-04-21 22:02:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Rahayu Chandra Bethania School</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Feather Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <!-- Navbar start -->
+    <nav class="navbar">
+      <a href="#" class="navbar-logo">Rahayu Chandra Bethania School</a>
+      <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">Profile</a>
+        <a href="menu.php">Kesiswaan</a>
+        <a href="supplier.php">Informasi</a>
+        <a href="#contact">Galeri</a>
+        <a href="masuk.php">Login</a>
+        <a href="gabung.php">Sign Up</a>
+      </div>
+
+      <div class="navbar-ekstra">
+      </div>
+    </nav>
+    <!-- Navbar end -->
+
+    <!-- Landing page start -->
+    <section class="landing" id="home">
+      <main class="content">
+        <h1>
+          Rahayu Chandra Bethania School
+        </h1>
+        <p>
+          Selamat datang di website resmi <strong>Rahayu Chandra Bethania School</strong>, sebuah lembaga pendidikan yang berkomitmen untuk mencetak generasi unggul, berakhlak mulia, dan siap bersaing di era global. Melalui situs ini, kami menyajikan berbagai informasi penting seputar kegiatan sekolah, struktur organisasi, layanan akademik, serta akses login untuk guru dan staf. Semoga website ini dapat menjadi sarana komunikasi yang efektif antara sekolah dengan seluruh warga sekolah maupun masyarakat luas.
+        </p>
+      </main>
+    </section>
+    <!-- Landing page end -->
+
+    <!-- About section start -->
+    <section id="about" class="about">
+      <h2>About Us</h2>
+
+      <div class="row">
+        <div class="about-img">
+          <img src="img/aboutus.jpg" alt="Tentang Kami" />
+        </div>
+        <div class="content">
+          <h3>Kenapa harus Rahayu Chandra Bethania School?</h3>
+          <p>
+          Rahayu Chandra Bethania School hadir sebagai lembaga pendidikan yang mengedepankan kualitas, karakter, dan pembinaan spiritual. 
+          Dengan tenaga pengajar yang profesional, lingkungan belajar yang nyaman, serta kurikulum yang terintegrasi dengan nilai-nilai moral dan keimanan, kami berkomitmen membentuk generasi masa depan yang cerdas, beretika, dan siap bersaing secara global. 
+          Di sini, setiap siswa diperlakukan sebagai pribadi istimewa yang layak untuk dibimbing, dikembangkan, dan dipersiapkan menjadi pemimpin masa depan.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!-- About section end -->
+
+    <!-- Contact section start -->
+    <section id="contact" class="contact">
+      <h2> Our Contacts!</h2>
+      <p>
+      Have questions or need assistance? We’re here to help! 
+      Contact us today:
+      </p>
+
+      <div class="row">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.301800130808!2d98.50204707496503!3d3.1205859531489615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303101b52350a26b%3A0x750eabade491d5fb!2sTOPC%20Kabanjahe!5e0!3m2!1sid!2sid!4v1713700000000!5m2!1sid!2sid"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="map"
+        ></iframe>
+        <div class="sosmed">
+          <i data-feather="instagram"> </i>
+          <h3>@bethania_school</h3>
+        </div>
+        <div class="sosmed">
+          <i data-feather="twitter"> </i>
+          <h3>@bethania_school</h3>
+        </div>
+        <div class="sosmed">
+          <i data-feather="mail"> </i>
+          <h3>bethania.school@gmail.com</h3>
+        </div>
+        <div class="sosmed">
+          <i data-feather="phone"> </i>
+          <h3>0857 6275 7879</h3>
+        </div>
+      </div>
+    </section>
+    <!-- Contact section end -->
+
+    <!-- Feather Icons -->
+    <script>
+      feather.replace();
+    </script>
+  </body>
+</html>
